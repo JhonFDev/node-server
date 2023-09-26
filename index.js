d92c63811d5577b7d197502da6c135b6599ddc29
JhonFDev <jhonfdeveloper@gmail.com>
2023-09-25 19:25:19 -0500

    agg async-await and then()

--- index.js
+++ index.js
@@ -3,38 +3,53 @@ const readlineSync = require("readline-sync");
 let listTask = [];
 
 function addTask() {
-  const indicator = readlineSync.question('Ingrese el indicador de la tarea: ');
-  const description = readlineSync.question('ingrese la descripcion de la tarea: ');
+  return new Promise((resolve, reject) => {
+    const indicator = readlineSync.question(
+      "Ingrese el indicador de la tarea: "
+    );
+    const description = readlineSync.question(
+      "ingrese la descripcion de la tarea: "
+    );
 
-  listTask.push({
-    indicator,
-    description,
-    completed: false
+    listTask.push({
+      indicator,
+      description,
+      completed: false,
+    });
+
+    resolve(console.log("Tarea agregada con exito"));
   });
-
-  console.log('Tarea agregada con exito');
 }
 
 function removeTask() {
-  const index = readlineSync.question('Ingrese el indice de la tarea a eliminar: ');
+  return new Promise((resolve, reject) => {
+    const index = readlineSync.question(
+      "Ingrese el indice de la tarea a eliminar: "
+    );
 
-  if (index >= 0 && index < listTask.length) {
-    listTask.splice(index, 1);
-    console.log('Tarea Eliminada con exito');
-  } else {
-    console.log('Indice no valido!');
-  }
+    if (index >= 0 && index < listTask.length) {
+      listTask.splice(index, 1);
+      resolve(console.log("Tarea Eliminada con exito"));
+    } else {
+     reject(console.log("Indice no valido!"));
+    }
+  });
 }
 
 function completeTask() {
-  const index = readlineSync.question('Ingrese el indice de la tarea a completar: ');
-
-  if (index >= 0 && index < listTask.length) {
-    listTask[index].completed = true;
-    console.log('Tarea compleatada con exito');
-  } else {
-    console.log('Indice no valido!');
-  }
+    return new Promise((resolve, reject) => {
+        
+        const index = readlineSync.question(
+          "Ingrese el indice de la tarea a completar: "
+        );
+      
+        if (index >= 0 && index < listTask.length) {
+          listTask[index].completed = true;
+         resolve( console.log("Tarea compleatada con exito"));
+        } else {
+          reject(console.log("Indice no valido!"));
+        }
+    })
 }
 
 function printTasks() {
@@ -42,55 +57,146 @@ function printTasks() {
   console.log("   ║  LISTA DE TAREAS : ║    ");
   console.log("   ╚════════════════════╝    ");
   listTask.forEach((listTask, index) => {
-    const status = listTask.completed ? '[✔]' : '[ ]';
-    console.log('╔════════════════════════════════════════════════════════════════╗');
-    console.log(`║   ${index}. ${status} ${listTask.indicator}: ${listTask.description}    `);
-    console.log('╚════════════════════════════════════════════════════════════════╝');
-
+    const status = listTask.completed ? "[✔]" : "[ ]";
+    console.log(
+      "╔════════════════════════════════════════════════════════════════╗"
+    );
+    console.log(
+      `║   ${index}. ${status} ${listTask.indicator}: ${listTask.description}    `
+    );
+    console.log(
+      "╚════════════════════════════════════════════════════════════════╝"
+    );
   });
 }
 
-
 function run() {
-    while (true) {
-    console.log('╔════════════════════════════════╗');
-    console.log('║     ╔════════════════════╗     ║');
-    console.log('║     ║ CONFIGURAR TAREAS  ║     ║');
-    console.log('║     ╚════════════════════╝     ║');
-    console.log('║                                ║');
-    console.log('║     Escoje una opcion:         ║');
-    console.log('║                                ║');
-    console.log('║   1. Crear una tarea           ║');
-    console.log('║   2. Eliminar una tarea        ║');
-    console.log('║   3. Completar una tarea       ║');
-    console.log('║   4. Impirimir lista de tareas ║');
-    console.log('║   5. Salir de la App           ║');
-    console.log('║                                ║');
-    console.log('║   Created by: Jhon Florez ▲    ║');
-    console.log('╚════════════════════════════════╝');
+  while (true) {
+    console.log("╔════════════════════════════════╗");
+    console.log("║     ╔════════════════════╗     ║");
+    console.log("║     ║ CONFIGURAR TAREAS  ║     ║");
+    console.log("║     ╚════════════════════╝     ║");
+    console.log("║                                ║");
+    console.log("║     Escoje una opcion:         ║");
+    console.log("║                                ║");
+    console.log("║   1. Crear una tarea           ║");
+    console.log("║   2. Eliminar una tarea        ║");
+    console.log("║   3. Completar una tarea       ║");
+    console.log("║   4. Impirimir lista de tareas ║");
+    console.log("║   5. Salir de la App           ║");
+    console.log("║                                ║");
+    console.log("║   Created by: Jhon Florez ▲    ║");
+    console.log("╚════════════════════════════════╝");
 
-    const option = readlineSync.question('Ingrese el numero de opcion: ');
+    const option = readlineSync.question("Ingrese el numero de opcion: ");
 
     switch (option) {
-      case '1':
-        addTask();
+      case "1":
+        addTask()
+          .then((mensaje) => {
+            console.log(mensaje);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
         break;
-      case '2':
-        removeTask();
+      case "2":
+        removeTask()
+          .then((mensaje) => {
+            console.log(mensaje);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
         break;
-      case '3':
-        completeTask();
+      case "3":
+        completeTask()
+          .then((mensaje) => {
+            console.log(mensaje);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
         break;
-      case '4':
+      case "4":
         printTasks();
         break;
-      case '5':
+      case "5":
         return;
       default:
-        console.log('Opcion no valida!');
+        console.log("Opcion no valida!");
     }
   }
 }
 run();
 
+// ejecutar y resolver las promesas con async/await
 
+// function run() {
+//   while (true) {
+//     console.log("╔════════════════════════════════╗");
+//     console.log("║     ╔════════════════════╗     ║");
+//     console.log("║     ║ CONFIGURAR TAREAS  ║     ║");
+//     console.log("║     ╚════════════════════╝     ║");
+//     console.log("║                                ║");
+//     console.log("║     Escoje una opcion:         ║");
+//     console.log("║                                ║");
+//     console.log("║   1. Crear una tarea           ║");
+//     console.log("║   2. Eliminar una tarea        ║");
+//     console.log("║   3. Completar una tarea       ║");
+//     console.log("║   4. Impirimir lista de tareas ║");
+//     console.log("║   5. Salir de la App           ║");
+//     console.log("║                                ║");
+//     console.log("║   Created by: Jhon Florez ▲    ║");
+//     console.log("╚════════════════════════════════╝");
+
+//     const option = readlineSync.question("Ingrese el numero de opcion: ");
+
+//     switch (option) {
+//       case "1":
+//         async function taskcrea() {
+//           try {
+//             const tareaCreada = await addTask("Nueva tarea");
+//             console.log(tareaCreada);
+//           } catch (error) {
+//             console.error(error);
+//           }
+//         }
+//         taskcrea();
+//         break;
+
+//       case "2":
+//         async function taskelim() {
+//           try {
+//             const tareaEliminada = await removeTask();
+//             console.log(tareaEliminada);
+//           } catch (error) {
+//             console.error(error);
+//           }
+//         }
+//         taskelim();
+//         break;
+
+//       case "3":
+//         async function taskcompl() {
+//           try {
+//             const tareaCompletada = await completeTask();
+//             console.log(tareaCompletada);
+//           } catch (error) {
+//             console.error(error);
+//           }
+//         }
+//         taskcompl();
+//         break;
+
+//       case "4":
+//         printTasks();
+//         break;
+//       case "5":
+//         return;
+//       default:
+//         console.log("Opcion no valida!");
+//     }
+//   }
+// }
+// run();
